8bfe13da7b73793bdd79581f9b998aebbb1617b1
Archie Fan <archiefan@gmail.com>
2022-07-29 18:27:10 +0100

    Add project files.

---
+++ InvokeWebAPIEF/Models/Crypto.cs
@@ -0,0 +1,11 @@
+﻿namespace InvokeWebAPIEF.Models
+{
+    public class Crypto
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
